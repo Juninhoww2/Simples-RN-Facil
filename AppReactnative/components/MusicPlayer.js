b5f8008b6473d3bf09de8f572ef2af912b8a4b19
juninhoww2 <alqadianoKira@gmail.com>
2021-10-15 12:23:21 -0300

    [update]

--- AppReactnative/components/MusicPlayer.js
+++ AppReactnative/components/MusicPlayer.js
@@ -1,36 +1,45 @@
 import React from 'react';
-import {View, Text, StyleSheet, SafeAreaView, Dimensions, TouchableOpacity, Image} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
 const MusicPlayer = () => {
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.mainContainer}>
-      <View style={styles.artworkWrapper}>
-        <Image source={require('../assets/atwork/download.jpeg')} style={styles.atworkImg} />
+      <View style={styles.mainContainer}>
+        <View style={styles.artworkWrapper}>
+          <Image
+            source={require('../assets/atwork/download.jpeg')}
+            style={styles.atworkImg}
+          />
+        </View>
       </View>
-    </View>
-    <View style={styles.bottomContainer}>
-    <View style={styles.bottomControls}>
-      <TouchableOpacity onPress={() => {}}>
-        <Ionicons name="heart-outline" size={30} color="#777777" />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {}}>
-        <Ionicons name="repeat-outline" size={30} color="#777777" />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {}}>
-        <Ionicons name="share-outline" size={30} color="#777777" />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => {}}>
-        <Ionicons name="ellipsis-horizontal" size={30} color="#777777" />
-      </TouchableOpacity>
-    </View>
-
-
-    </View>
+      <View style={styles.bottomContainer}>
+        <View style={styles.bottomControls}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#777777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="repeat-outline" size={30} color="#777777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#777777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="ellipsis-horizontal" size={30} color="#777777" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -45,19 +54,18 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
-  bottomContainer: 
-    {
-      borderTopColor: '#393E46',
-      borderTopWidth: 1,
-      width: width,
-      alignItems: 'center',
-      paddingVertical: 15
-    },
+  bottomContainer: {
+    borderTopColor: '#393E46',
+    borderTopWidth: 1,
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+  },
   bottomControls: {
-    flexDirection: 'row', 
-    justifyContent: 'space-between', 
-    width:'80%'
-  }
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
 });
